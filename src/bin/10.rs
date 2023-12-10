e3e44d2edefeb57416721dddec256589c5b4bfa8
stianStensli <stian.hs@hotmail.com>
2023-12-10 17:55:27 +0100

    test

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -398,7 +398,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(4));
+        assert_eq!(result, Some(8));
     }
 
     #[test]
