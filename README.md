f617d07084ae3ae87758b452fc2dbf514c0c9566
stianStensli <stian.hs@hotmail.com>
2023-12-12 22:55:48 +0100

    day 12 part 1

--- README.md
+++ README.md
@@ -27,19 +27,20 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `92.1µs` | `831.0µs` |
-| [Day 2](./src/bin/02.rs) | `76.2µs` | `78.5µs` |
-| [Day 3](./src/bin/03.rs) | `224.2µs` | `153.0µs` |
-| [Day 4](./src/bin/04.rs) | `83.4µs` | `82.7µs` |
-| [Day 5](./src/bin/05.rs) | `73.5µs` | `131.3µs` |
-| [Day 6](./src/bin/06.rs) | `491.0ns` | `481.0ns` |
-| [Day 7](./src/bin/07.rs) | `204.1µs` | `210.7µs` |
-| [Day 8](./src/bin/08.rs) | `275.6µs` | `513.1µs` |
-| [Day 9](./src/bin/09.rs) | `687.1µs` | `713.1µs` |
-| [Day 10](./src/bin/10.rs) | `539.1µs` | `1.7ms` |
-| [Day 11](./src/bin/11.rs) | `330.6µs` | `439.2µs` |
+| [Day 1](./src/bin/01.rs) | `81.3µs` | `772.5µs` |
+| [Day 2](./src/bin/02.rs) | `70.6µs` | `70.7µs` |
+| [Day 3](./src/bin/03.rs) | `189.5µs` | `142.8µs` |
+| [Day 4](./src/bin/04.rs) | `78.3µs` | `73.3µs` |
+| [Day 5](./src/bin/05.rs) | `62.7µs` | `116.2µs` |
+| [Day 6](./src/bin/06.rs) | `560.0ns` | `462.0ns` |
+| [Day 7](./src/bin/07.rs) | `183.5µs` | `191.7µs` |
+| [Day 8](./src/bin/08.rs) | `243.8µs` | `476.4µs` |
+| [Day 9](./src/bin/09.rs) | `617.4µs` | `673.1µs` |
+| [Day 10](./src/bin/10.rs) | `449.0µs` | `1.4ms` |
+| [Day 11](./src/bin/11.rs) | `305.5µs` | `407.1µs` |
+| [Day 12](./src/bin/12.rs) | `20.8ms` | `-` |
 
-**Total: 7.44ms**
+**Total: 27.41ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,148 +1,157 @@
 advent_of_code::solution!(12);
 
 
-fn place_first(input: &str, nr_groups: &Vec<u32>) -> u32 {
-    let mut has_placed_main = false;
-    let mut nr_of_possible = 0;
-
-    let mut next_problem = input.chars().map(|c| {
-        if !has_placed_main && c == '?' {
-            has_placed_main = true;
-            '#'
-        } else {
-            c
+fn place_first(input: Option<Vec<char>>, nr_groups: &Vec<u32>) -> u64 {
+    if input.is_none() {
+        return 0;
+    }
+    let input = input.unwrap();
+    //let input_clone: String = input.clone().iter().collect();
+    if !input.contains(&'?') {
+        if is_possible(input, nr_groups) {
+            //  println!("possible: {}", input_clone);
+            return 1;
         }
-    }).collect::<String>();
-
-
-    while next_problem.contains('?') && !is_definitly_not_possible(next_problem.as_str(), &nr_groups) {
-        nr_of_possible += place_first(next_problem.as_str(), nr_groups);
-        let mut has_placed = false;
-        next_problem = next_problem.chars().map(|c| {
-            if !has_placed && c == '?' {
-                has_placed = true;
-                '.'
-            } else {
-                c
-            }
-        }).collect::<String>();
+        //println!("not possible: {}", input_clone);
+        return 0;
     }
-
-    if is_possible(next_problem.as_str(), nr_groups) {
-        nr_of_possible += 1;
-    }
-    nr_of_possible
+    place_first(next_move_good(input.clone(), nr_groups, true), nr_groups) +
+        place_first(next_move_good(input, nr_groups, false), nr_groups)
 }
 
 // ????????.##.?.?##..??? 1,2,3
 // s:(8,0),s:(2,2), s:(1,0), s:(3,2), s:(3,0)
 
 
-#[derive(Debug)]
-struct GNode {
-    size: u32,
-    forced: u32,
-}
+fn is_possible(problem: Vec<char>, nr_groups: &Vec<u32>) -> bool {
+    let mut g_index = 0;
+    let mut numb_gear = 0;
 
-fn is_possible(problem: &str, nr_groups: &Vec<u32>) -> bool {
-    let mut groups: Vec<u32> = Vec::new();
-    problem.split('.').for_each(|s| {
-        if !s.is_empty() {
-            groups.push(s.len() as u32);
-        }
-    });
-    if groups.len() != nr_groups.len() {
-        return false;
-    }
-
-    groups.iter().zip(nr_groups.iter()).all(|(springs, springs_real)| springs == springs_real)
-}
-
-/*
-
-    let groups_size: Vec<u32> = problem.split('.').map(|s| s.len() as u32).collect();
-    let groups_forced: Vec<u32> = problem.split(['.', '?']).map(|s| s.len() as u32).filter(|x| x > &0).collect();
-
-    let nr_of_springs: u32 = groups_forced.iter().sum();
-    let nr_of_poss: u32 = groups_size.iter().sum();
-    let nr_of_springs_real: u32 = nr_groups.iter().sum();
-    if nr_of_springs > nr_of_springs_real || nr_of_poss < nr_of_springs_real {
-        return true;
-    }
- */
-fn is_definitly_not_possible(problem: &str, nr_groups: &Vec<u32>) -> bool {
-    // #.#.### 1,1,3
-    let groups: Vec<GNode> = problem.split('.').map(|s| {
-        GNode {
-            size: s.len() as u32,
-            forced: s.chars().filter(|c| c == &'#').count() as u32,
-        }
-    }).collect();
-    let nr_of_springs: u32 = groups.iter().map(|g| g.forced).sum();
-    let nr_of_poss: u32 = groups.iter().map(|g| g.size).sum();
-    let nr_of_springs_real: u32 = nr_groups.iter().sum();
-    if nr_of_springs > nr_of_springs_real || nr_of_poss < nr_of_springs_real {
-        return true;
-    }
-
-    /*
-    let mut index = 0;
-    //groups.iter().map(|g|g.forced).max().unwrap() > *nr_groups.iter().max().unwrap()
-    //false
-    let r = groups_forced.iter()
-        .any(|g| {
-            for i in index..nr_groups.len() {
-                index = i + 1;
-                if *g <= nr_groups[i] {
+    //let input_clone: String = problem.clone().iter().collect();
+    //println!("prob: {}", input_clone);
+    for i in 0..problem.len() {
+        if problem[i] == '.' {
+            if numb_gear > 0 {
+                //println!("Sceck: i {}, g_i: {}, gears: {}", i, g_index, numb_gear);
+                if g_index >= nr_groups.len() {
                     return false;
                 }
+                if nr_groups[g_index] != numb_gear {
+                    //println!("r: {:?}", nr_groups);
+                    // println!("wrong: {},{}, {}",g_index,nr_groups[g_index],numb_gear);
+                    return false;
+                }
+                g_index += 1;
             }
-            if index == nr_groups.len() {
-                return true;
-            }
-            false
-        });
-
-    println!("r: {}, prob: {}, nr_groups: {:?}, g: {:?}", r, problem, nr_groups, groups_forced);
-    r*/
-    false
+            numb_gear = 0;
+        } else {
+            numb_gear += 1;
+        }
+    }
+    if numb_gear > 0 {
+        if g_index >= nr_groups.len() {
+            return false;
+        }
+        if nr_groups[g_index] != numb_gear {
+            return false;
+        }
+        g_index += 1;
+    }
+    if g_index != nr_groups.len() {
+        return false;
+    }
+    true
 }
 
+fn next_move_good(problem: Vec<char>, nr_groups: &Vec<u32>, place: bool) -> Option<Vec<char>> {
+    let mut chars: Vec<char> = problem;
+    let mut index = 0;
+    let mut nr_of_gears = 0;
+    let mut is_pre_init = true;
 
-fn main_stuff(input: &str) -> u32 {
-    //println!("problem: {}", input);
-    let mut input = input.split(' ');
-    let problem: &str = input.next().unwrap();
-    let nr_groups: Vec<u32> = input.next().unwrap().split(',').map(|e| e.parse::<u32>().unwrap()).collect();
-
-    let mut next_problem: String = problem.to_string();
-    let mut nr_of_possible = 0;
-
-    while next_problem.contains('?') && !is_definitly_not_possible(next_problem.as_str(), &nr_groups) {
-        nr_of_possible += place_first(next_problem.as_str(), &nr_groups);
-        let mut has_placed = false;
-        next_problem = next_problem.chars().map(|c| {
-            if !has_placed && c == '?' {
-                has_placed = true;
-                '.'
+    for i in 0..chars.len() {
+        let c = chars[i];
+        if c == '#' {
+            nr_of_gears += 1;
+            is_pre_init = false;
+        } else if c == '.' && nr_of_gears > 0 {
+            index += 1;
+            nr_of_gears = 0;
+        } else if c == '?' {
+            return if place {
+                if index < nr_groups.len() && nr_of_gears < nr_groups[index] {
+                    chars[i] = '#';
+                    Some(chars)
+                } else {
+                    None
+                }
             } else {
-                c
-            }
-        }).collect::<String>();
+                /*
+                if  index < nr_groups.len() {
+                    let mut rest_sum = nr_groups[index];
+                    for j in index + 1..nr_groups.len() {
+                        rest_sum += nr_groups[j] + 1;
+                    }
+                    if rest_sum > (chars.len() - i) as u32{
+                        let p: String = chars.iter().collect();
+                        //println!("prob: {}", p);
+                        //println!("rest_sum: {}, i: {}, c: {}", rest_sum, i, chars.len());
+                        return None
+                    }
+                }
+                 */
+                if is_pre_init ||
+                    (i > 0 && chars[i - 1] == '.') ||
+                    (index < nr_groups.len() && nr_of_gears == nr_groups[index]) {
+                    chars[i] = '.';
+                    Some(chars)
+                } else {
+                    //chars[i] = '.';
+                    //let r: String = chars.iter().collect();
+                    //println!("Nope: {}", r);
+                    None
+                }
+            };
+        }
     }
-    if is_possible(next_problem.as_str(), &nr_groups) {
-        //println!("Is possible: {}, {:?}", next_problem, nr_groups);
-        nr_of_possible += 1;
-    }
-
-    nr_of_possible
+    None
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+
+fn main_stuff(input: &str) -> u64 {
+    //println!("Main: {}", input);
+    let mut input = input.split(' ');
+    let problem: Vec<char> = input.next().unwrap().chars().collect();
+    let nr_groups: Vec<u32> = input.next().unwrap().split(',').map(|e| e.parse::<u32>().unwrap()).collect();
+    let res = place_first(Some(problem), &nr_groups);
+    //println!("res: {}", res);
+    res
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
     Some(input.lines().map(main_stuff).sum())
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
+fn fold_5(input: &str) -> String {
+    let mut r = input.split(' ');
+    let prob = r.next().unwrap();
+    let digs = r.next().unwrap();
+    let mut new_prob = prob.to_string();
+    let mut new_digs = digs.to_string();
+    for _ in 0..4 {
+        new_prob.push('?');
+        new_digs.push(',');
+        new_prob.push_str(prob);
+        new_digs.push_str(digs);
+    }
+    new_prob.push(' ');
+    new_prob.push_str(new_digs.as_str());
+
+    new_prob
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    //Some(input.lines().map(fold_5).map(|s| main_stuff(s.as_str())).sum())
     None
 }
 
@@ -160,5 +169,6 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
+        //assert_eq!(None, None);
     }
 }
