63e84c39d9f562765734ccfcac90f0b99c8b68a5
stianStensli <stian.hs@hotmail.com>
2023-12-13 16:20:45 +0100

    day 13 part 1 and 2

--- README.md
+++ README.md
@@ -28,20 +28,21 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `89.5µs` | `832.5µs` |
-| [Day 2](./src/bin/02.rs) | `76.5µs` | `73.0µs` |
-| [Day 3](./src/bin/03.rs) | `199.3µs` | `151.1µs` |
-| [Day 4](./src/bin/04.rs) | `78.7µs` | `73.7µs` |
-| [Day 5](./src/bin/05.rs) | `68.7µs` | `118.2µs` |
-| [Day 6](./src/bin/06.rs) | `519.0ns` | `499.0ns` |
-| [Day 7](./src/bin/07.rs) | `188.4µs` | `188.1µs` |
-| [Day 8](./src/bin/08.rs) | `259.4µs` | `465.4µs` |
-| [Day 9](./src/bin/09.rs) | `622.6µs` | `667.5µs` |
-| [Day 10](./src/bin/10.rs) | `450.7µs` | `1.4ms` |
-| [Day 11](./src/bin/11.rs) | `300.5µs` | `425.2µs` |
-| [Day 12](./src/bin/12.rs) | `13.5ms` | `-` |
+| [Day 1](./src/bin/01.rs) | `91.3µs` | `862.5µs` |
+| [Day 2](./src/bin/02.rs) | `77.4µs` | `79.7µs` |
+| [Day 3](./src/bin/03.rs) | `225.4µs` | `153.0µs` |
+| [Day 4](./src/bin/04.rs) | `75.5µs` | `77.3µs` |
+| [Day 5](./src/bin/05.rs) | `64.1µs` | `120.6µs` |
+| [Day 6](./src/bin/06.rs) | `360.0ns` | `403.0ns` |
+| [Day 7](./src/bin/07.rs) | `220.0µs` | `200.4µs` |
+| [Day 8](./src/bin/08.rs) | `254.1µs` | `484.3µs` |
+| [Day 9](./src/bin/09.rs) | `629.7µs` | `679.3µs` |
+| [Day 10](./src/bin/10.rs) | `471.7µs` | `1.4ms` |
+| [Day 11](./src/bin/11.rs) | `310.5µs` | `407.7µs` |
+| [Day 12](./src/bin/12.rs) | `13.7ms` | `-` |
+| [Day 13](./src/bin/13.rs) | `381.9µs` | `422.0µs` |
 
-**Total: 20.23ms**
+**Total: 21.39ms**
 <!--- benchmarking table --->
 
 ---
---
+++ src/bin/13.rs
@@ -0,0 +1,157 @@
+advent_of_code::solution!(13);
+
+pub fn mirror_index(prob: &Vec<Vec<bool>>) -> u32 {
+    let mut is_col_not_mirror: Vec<bool> = Vec::new();
+
+    prob[0].iter().skip(1).for_each(|_| is_col_not_mirror.push(false));
+    for i in 0..prob.len() {
+        for j in 0..prob[i].len() - 1 {
+            let mut mirr_index = j + 1;
+            let mut mirr_rev_index = j;
+            let mut is_not_mirror = false;
+            if !is_col_not_mirror[j] {
+                while mirr_index < prob[i].len() {
+                    if prob[i][mirr_index] != prob[i][mirr_rev_index] {
+                        is_not_mirror = true;
+                        break;
+                    }
+                    if mirr_rev_index == 0 {
+                        break;
+                    }
+                    mirr_index += 1;
+                    mirr_rev_index -= 1;
+                }
+            }
+            if is_not_mirror {
+                is_col_not_mirror[j] = true;
+            }
+        }
+    }
+    for i in 0..is_col_not_mirror.len() {
+        if !is_col_not_mirror[i] {
+            return (i + 1) as u32;
+        }
+    }
+    0
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut prob: Vec<Vec<bool>> = Vec::new();
+    let mut prob_flip: Vec<Vec<bool>> = Vec::new();
+    let mut sum = 0;
+    input.lines().for_each(|l| {
+        if l.is_empty() && !prob.is_empty() {
+            let m = mirror_index(&prob);
+            if m > 0 {
+                sum += m;
+            } else {
+                sum += 100 * mirror_index(&prob_flip);
+            }
+            prob.clear();
+            prob_flip.clear()
+        } else {
+            let mut p_r = Vec::new();
+            l.chars().for_each(|c| {
+                if prob_flip.len() == p_r.len() {
+                    prob_flip.push(Vec::new());
+                }
+                prob_flip[p_r.len()].push(c == '.');
+                p_r.push(c == '.');
+            });
+            prob.push(p_r);
+        }
+    });
+    let m = mirror_index(&prob);
+    if m > 0 {
+        sum += m;
+    }else {
+        sum += 100 * mirror_index(&prob_flip);
+    }
+    Some(sum)
+}
+
+
+pub fn mirror_index_smudge(prob: &Vec<Vec<bool>>) -> u32 {
+    let mut smudge_mirror: Vec<u32> = Vec::new();
+
+    prob[0].iter().skip(1).for_each(|_| smudge_mirror.push(0));
+    for i in 0..prob.len() {
+        for j in 0..prob[i].len() - 1 {
+            let mut mirr_index = j + 1;
+            let mut mirr_rev_index = j;
+            if !smudge_mirror[j] > 1 {
+                while mirr_index < prob[i].len() && smudge_mirror[j] <= 1{
+                    if prob[i][mirr_index] != prob[i][mirr_rev_index] {
+                        smudge_mirror[j] += 1;
+                        break;
+                    }
+                    if mirr_rev_index == 0 {
+                        break;
+                    }
+                    mirr_index += 1;
+                    mirr_rev_index -= 1;
+                }
+            }
+        }
+    }
+    //println!("smudge: {:?}",smudge_mirror);
+    for i in 0..smudge_mirror.len() {
+        if smudge_mirror[i] == 1 {
+            return (i + 1) as u32;
+        }
+    }
+    0
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+
+    let mut prob: Vec<Vec<bool>> = Vec::new();
+    let mut prob_flip: Vec<Vec<bool>> = Vec::new();
+    let mut sum = 0;
+    input.lines().for_each(|l| {
+        if l.is_empty() && !prob.is_empty() {
+            let m = mirror_index_smudge(&prob);
+            if m > 0 {
+                sum += m;
+            } else {
+                sum += 100 * mirror_index_smudge(&prob_flip);
+            }
+            prob.clear();
+            prob_flip.clear()
+        } else {
+            let mut p_r = Vec::new();
+            l.chars().for_each(|c| {
+                if prob_flip.len() == p_r.len() {
+                    prob_flip.push(Vec::new());
+                }
+                prob_flip[p_r.len()].push(c == '.');
+                p_r.push(c == '.');
+            });
+            prob.push(p_r);
+        }
+    });
+    let m = mirror_index_smudge(&prob);
+    if m > 0 {
+        sum += m;
+    }else {
+        sum += 100 * mirror_index_smudge(&prob_flip);
+    }
+    Some(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(5));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(400));
+    }
+}
