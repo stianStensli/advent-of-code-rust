75982f6572debfb128f828f941c1e9675f016191
stianStensli <stian.hs@hotmail.com>
2023-12-13 21:03:46 +0100

    day 12 part 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,24 +1,18 @@
 advent_of_code::solution!(12);
+use cached::proc_macro::cached;
 
-fn place_first_group(input: &Vec<Vec<bool>>, nr_groups: &Vec<u32>) -> u32 {
-    println!("---");
-    println!("in: {:?}",input);
-    println!("g: {:?}",nr_groups);
-    println!("---");
+#[cached]
+fn place_first_group(input: Vec<Vec<bool>>, nr_groups: Vec<u32>) -> u64 {
     if input.is_empty() {
         if nr_groups.is_empty() {
-            println!("yes!!");
             return 1;
         }
-        println!("no: nr_g: {:?}", nr_groups);
         return 0;
     }
     if nr_groups.is_empty() {
         if input.iter().all(|v| v.iter().all(|w| !*w)) {
-            println!("yes!!");
             return 1;
         }
-        println!("nope input: {:?}", input);
         return 0;
     }
     let place_group = input.first().unwrap().clone();
@@ -31,7 +25,6 @@ fn place_first_group(input: &Vec<Vec<bool>>, nr_groups: &Vec<u32>) -> u32 {
         if !place_group[i] {
             // hvis nr_of_springs er mer enn neste gruppe: ##x 1
             if nr_of_springs > next_nr_group {
-                println!("too many: . input: {:?}, g: {:?}", input, nr_groups);
                 return res;
             }
             if nr_of_springs == 0 || nr_of_springs == next_nr_group{
@@ -47,22 +40,19 @@ fn place_first_group(input: &Vec<Vec<bool>>, nr_groups: &Vec<u32>) -> u32 {
                     next_groups.push(input[j].clone())
                 }
                 if i == place_group.len() - 1 {
-                    res += place_first_group(&next_groups, &next_nr_groups);
+                    res += place_first_group(next_groups, next_nr_groups);
                 } else {
                     let mut next_prob = Vec::new();
                     for j in i + 1..place_group.len() {
                         next_prob.push(place_group[j])
                     }
                     next_groups.insert(0, next_prob);
-                    res += place_first_group(&next_groups, &next_nr_groups)
+                    res += place_first_group(next_groups, next_nr_groups)
                 }
-            } else {
-                println!("ignoring . placement");
             }
             // place group...
             nr_of_springs += 1;
             if nr_of_springs > next_nr_group {
-                println!("no: # input: {:?}, g: {:?}", input, nr_groups);
                 return res
             }
 
@@ -73,15 +63,11 @@ fn place_first_group(input: &Vec<Vec<bool>>, nr_groups: &Vec<u32>) -> u32 {
             }
             if i == place_group.len() - 1 {
                 if nr_of_springs != next_nr_group {
-                    println!("no happy: # input: {:?}, g: {:?}", input, nr_groups);
                     return res
                 }
-                //happy!
-
+                //happy placed!
                 next_nr_groups.remove(0);
-                println!("happy placed: p_{:?}, g_{:?}", input, nr_groups);
-                println!("happy placed?: p_{:?}, g_{:?}", next_groups, next_nr_groups);
-                res += place_first_group(&next_groups, &next_nr_groups);
+                res += place_first_group(next_groups, next_nr_groups);
                 return res
             } else {
                 let mut next_prob = Vec::new();
@@ -93,9 +79,7 @@ fn place_first_group(input: &Vec<Vec<bool>>, nr_groups: &Vec<u32>) -> u32 {
                     }
                 }
                 next_groups.insert(0, next_prob);
-                println!("just placed: p_{:?}, g_{:?}", input, nr_groups);
-                println!("just placed?: p_{:?}, g_{:?}", next_groups, next_nr_groups);
-                res += place_first_group(&next_groups, &next_nr_groups);
+                res += place_first_group(next_groups, next_nr_groups);
 
                 return res
             }
@@ -110,15 +94,12 @@ fn place_first_group(input: &Vec<Vec<bool>>, nr_groups: &Vec<u32>) -> u32 {
         for j in 1..input.len() {
             next_groups.push(input[j].clone())
         }
-        println!("trying: p_{:?}, g_{:?}", input, nr_groups);
-        res += place_first_group(&next_groups, &next_nr_groups)
+        res += place_first_group(next_groups, next_nr_groups)
     }
-    println!("done!");
     res
 }
 
-fn main_stuff(input: &str) -> u32 {
-    println!("Main: {} \n", input);
+fn main_stuff(input: &str) -> u64 {
     let mut input = input.split(' ');
     let problem_str: &str = input.next().unwrap();
     let groups: Vec<Vec<bool>> = problem_str
@@ -128,26 +109,13 @@ fn main_stuff(input: &str) -> u32 {
             .map(|c| c == '#')
             .collect()
         ).collect();
-    groups.iter().for_each(|i| {
-        i.iter().for_each(|v| {
-            if *v {
-                print!("#")
-            } else {
-                print!("?")
-            }
-        });
-        println!("");
-    });
 
     let nr_groups: Vec<u32> = input.next().unwrap().split(',').map(|e| e.parse::<u32>().unwrap()).collect();
 
-    let res = place_first_group(&groups,&nr_groups);
-    println!("Problem: {}",problem_str);
-    println!("result: {}",res);
-    res
+    place_first_group(groups,nr_groups)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     Some(input.lines().map(main_stuff).sum())
 }
 
@@ -170,8 +138,7 @@ fn fold_5(input: &str) -> String {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    //Some(input.lines().map(fold_5).map(|s| main_stuff(s.as_str())).map(|a| a as u64).sum())
-    None
+    Some(input.lines().map(fold_5).map(|s| main_stuff(s.as_str())).map(|a| a as u64).sum())
 }
 
 #[cfg(test)]
@@ -187,7 +154,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-        //assert_eq!(None, None);
+        assert_eq!(result, Some(525152));
     }
 }
