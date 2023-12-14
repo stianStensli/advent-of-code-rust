a9bbfb7f3c4ae4a432ee4bd063dac3d617197473
stianStensli <stian.hs@hotmail.com>
2023-12-14 15:31:24 +0100

    dag 14 part 2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,4 +1,115 @@
+use cached::proc_macro::cached;
 advent_of_code::solution!(14);
+
+#[cached]
+fn tilt_east(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut stones_2: Vec<Vec<char>> = vec![vec!['.'; input[0].len()]; input.len()];
+    let mut r_index = 0;
+    input.iter().for_each(|l| {
+        let mut c_index = l.len() - 1;
+        let mut last_pos = l.len() - 1;
+        l.iter().rev().for_each(|c| {
+            if *c == 'O' {
+                stones_2[r_index][last_pos] = 'O';
+                last_pos = last_pos.saturating_sub(1);
+            } else if *c == '#' {
+                stones_2[r_index][c_index] = '#';
+                last_pos = c_index.saturating_sub(1);
+            }
+            c_index = c_index.saturating_sub(1);
+        });
+        r_index += 1;
+    });
+    /*
+    println!("\nloop");
+    stones_2.iter().for_each(|l| {
+        print!("\n");
+        l.iter().for_each(|v| print!("{}",v))
+    });*/
+    stones_2
+}
+
+#[cached]
+fn tilt_west(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut stones_2: Vec<Vec<char>> = vec![vec!['.'; input[0].len()]; input.len()];
+    let mut r_index = 0;
+    input.iter().for_each(|l| {
+        let mut c_index = 0;
+        let mut last_pos = 0;
+        l.iter().for_each(|c| {
+            if *c == 'O' {
+                stones_2[r_index][last_pos] = 'O';
+                last_pos += 1;
+            } else if *c == '#' {
+                stones_2[r_index][c_index] = '#';
+                last_pos = c_index + 1;
+            }
+            c_index += 1;
+        });
+        r_index += 1;
+    });
+    stones_2
+}
+
+#[cached]
+fn tilt_south(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut stones_2: Vec<Vec<char>> = vec![vec!['.'; input[0].len()]; input.len()];
+
+    for col in 0..input[0].len() {
+        let mut row = input.len() - 1;
+        let mut last_pos = row;
+        loop {
+            let c = input[row][col];
+            if c == 'O' {
+                stones_2[last_pos][col] = 'O';
+                last_pos = last_pos.saturating_sub(1);
+            } else if c == '#' {
+                stones_2[row][col] = '#';
+                last_pos = row.saturating_sub(1);
+            }
+            if row == 0 {
+                break;
+            } else {
+                row -= 1;
+            }
+        }
+    }
+    stones_2
+}
+
+#[cached]
+fn tilt_north(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let max_row = input[0].len();
+    let mut stones_2: Vec<Vec<char>> = vec![Vec::new(); max_row];
+    let mut r_index = 0;
+    input.iter().for_each(|l| {
+        let mut c_index = 0;
+        l.iter().for_each(|c| {
+            if *c == 'O' {
+                stones_2[c_index].push('O');
+            } else if *c == '#' {
+                while stones_2[c_index].len() < r_index {
+                    stones_2[c_index].push('.');
+                }
+                stones_2[c_index].push('#');
+            }
+            c_index += 1;
+        });
+        r_index += 1;
+    });
+    let mut res: Vec<Vec<char>> = vec![Vec::new(); input.len()];
+    for i in 0..res.len() {
+        for j in 0..max_row {
+            let s = &stones_2[j];
+            if s.len() > i {
+                res[i].push(stones_2[j][i]);
+            } else {
+                res[i].push('.');
+            }
+        }
+    }
+    res
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
     let max_row = input.lines().next().unwrap().len();
@@ -24,8 +135,30 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(res)
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+fn tumble(input: Vec<Vec<char>>, n: u32) -> u32 {
+    let mut tumbled_value = input;
+    let max_row = tumbled_value[0].len() as u32;
+    for _ in 0..n {
+        tumbled_value = tilt_east(tilt_south(tilt_west(tilt_north(tumbled_value))));
+    }
+    let mut r_index: u32 = 0;
+    tumbled_value.iter().map(|l| {
+        let r: u32 = l.iter().map(|v| {
+            let o = if *v == 'O' {
+                max_row - r_index
+            } else {
+                0
+            };
+            o
+        }).sum();
+        r_index += 1;
+        r
+    }).sum()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    //102657
+    Some(tumble(input.lines().map(|l| l.chars().collect()).collect(), 1000))
 }
 
 #[cfg(test)]
@@ -41,6 +174,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(64));
     }
 }
