2eee533d49e01170ee164e464d908cb55f1dab46
stianStensli <stian.hs@hotmail.com>
2023-12-14 13:26:04 +0100

    dag 14 part 1 opt

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,36 +1,27 @@
 advent_of_code::solution!(14);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut stones_2: Vec<Vec<bool>> = vec![Vec::new(); input.lines().next().unwrap().len()];
+    let max_row = input.lines().next().unwrap().len();
+    let mut stones_index: Vec<u32> = vec![0; max_row];
+    let max_row: u32 = max_row as u32;
+
     let mut r_index = 0;
+    let mut res = 0;
     input.lines().for_each(|l| {
         let mut c_index = 0;
         l.chars().for_each(|c| {
             if c == 'O' {
-                stones_2[c_index].push(true);
+                let old_index = stones_index[c_index];
+                res += max_row - old_index;
+                stones_index[c_index] += 1;
             } else if c == '#' {
-                while stones_2[c_index].len() <= r_index {
-                    stones_2[c_index].push(false);
-                }
+                stones_index[c_index] = r_index + 1;
             }
             c_index += 1;
         });
         r_index += 1;
     });
-    let max_row = r_index as u32;
-    r_index = 0;
-    Some(stones_2.iter().map(|s| {
-        let mut res: u32 = 0;
-
-        r_index = 0;
-        s.iter().for_each(|v| {
-            if *v {
-                res += max_row - r_index as u32;
-            }
-            r_index += 1;
-        });
-        res
-    }).sum())
+    Some(res)
 }
 
 pub fn part_two(_: &str) -> Option<u32> {
@@ -43,7 +34,7 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(136));
     }
 
