94900544cd842692545111d63eb2a79e3e9e96d2
stianStensli <stian.hs@hotmail.com>
2023-12-15 16:33:10 +0100

    dag 15 part 2

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -99,7 +99,7 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(510388));
     }
 
